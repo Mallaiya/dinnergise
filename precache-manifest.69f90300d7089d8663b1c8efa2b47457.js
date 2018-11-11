84e85f50f837ee619366e6b65b6797e9d1cdeeff
Mallaiya M <mallaiyazap@gmail.com>
2018-11-11 13:25:16 +0530

    Updates

---
+++ precache-manifest.69f90300d7089d8663b1c8efa2b47457.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/dinnergise/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "36af4a37f5d3bcc12a93",
+    "url": "/dinnergise/static/css/main.3aa2ef61.chunk.css"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/dinnergise/static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "dfb59f7d17e72fb1690a",
+    "url": "/dinnergise/static/js/1.dfb59f7d.chunk.js"
+  },
+  {
+    "revision": "297381e8130649fdfa10",
+    "url": "/dinnergise/static/js/runtime~main.297381e8.js"
+  },
+  {
+    "revision": "05158509544a82d79ef5536b3ad45a7a",
+    "url": "/dinnergise/static/media/navLogo.05158509.png"
+  },
+  {
+    "revision": "a8f4b8f37499408781351476308a60a8",
+    "url": "/dinnergise/static/media/banner.a8f4b8f3.jpg"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/dinnergise/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/dinnergise/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/dinnergise/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/dinnergise/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "36af4a37f5d3bcc12a93",
+    "url": "/dinnergise/static/js/main.36af4a37.chunk.js"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/dinnergise/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/dinnergise/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/dinnergise/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/dinnergise/static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/dinnergise/static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/dinnergise/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/dinnergise/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/dinnergise/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/dinnergise/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "dfb59f7d17e72fb1690a",
+    "url": "/dinnergise/static/css/1.6a06e4b5.chunk.css"
+  },
+  {
+    "revision": "2460ed89f100fe316d34bf6b48cc4f27",
+    "url": "/dinnergise/index.html"
+  }
+];
